da46536444d96cddbf9a9460562f6f3b27a5ceec
MalikT <maliktremain1@gmail.com>
2024-12-20 05:21:18 +0000

    fixed easter egg

--- bot.py
+++ bot.py
@@ -69,11 +69,11 @@ async def player_stats(ctx, player: str):
     """Display stats for a specific player."""
     stats = elo_system.calculate_stats_from_history()
     player = player.lower()
-    if player not in elo_system.players:
-        await ctx.send(f"No data found for player `{player}`.")
-        return
     if(player == "tilted"):
         await ctx.send("```Stats for tilted\n- Elo Rating:999999999\n- Games Played: 69\n- Wins: 69\n- Losses: 0\n- Win Rate: 100%")
+        return
+    if player not in elo_system.players:
+        await ctx.send(f"No data found for player `{player}`.")
         return
     rating = round(elo_system.players[player], 2)
     player_stats = stats[player]
